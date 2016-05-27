229901d198ee255ee2987867e622fa79ad230fbd
nomadMystic <nomadmystics@gmail.com>
2016-05-26 21:11:28 -0700

    Created one method for the ImageSet class, added two testing images inside new folder includes/images, and created temporary Imagick class for displaying images. [BUG] GIF not animation not functional(Look in Imageset class method CreatePageImage() for configuration of $frame->setIamgePage Imagick class method

--- week6/assets/includes/ImageSet.php
+++ week6/assets/includes/ImageSet.php
@@ -9,6 +9,30 @@
 
 class ImageSet
 {
+    // sharing constants
     const SHARING_PRIVATE = 'private';
     const SHARING_PUBLIC = 'public';
+
+    const PAGE_IMAGE_WIDTH = 800;
+    const THUMBNAIL_IMAGE_WIDTH = 128;
+    const THUMBNAIL_IMAGE_HEIGHT = 128;
+
+
+    public function createPageImage($source_image)
+    {
+        $image = $source_image->coalesceImages();
+        $data = $image->getImageGeometry();
+
+        foreach ($image as $frame) {
+            $frame->scaleImage(ImageSet::PAGE_IMAGE_WIDTH, 0);
+            // positioning images at absolute 0 0 0 0
+            $frame->setImagePage(0, 0, 0, 0);
+        }
+
+        $page_blob = $image->getImageBlob();
+        $page_data = $image->getImageGeometry();
+
+        return $page_blob;
+    }
+
 }
